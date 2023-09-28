10b603e6d7e4a47199ca225166e227b515e7429b
sattara1985 <44877707+sattara1985@users.noreply.github.com>
2023-09-28 11:55:52 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user:"root",
+    password : "",  //enter your password here
+    database: "empleados_crud"
+});
+
+app.post("/create",(req,res)=>{
+    const nombre=req.body.nombre;
+    const edad=req.body.edad;
+    const pais=req.body.pais;
+    const cargo=req.body.cargo;
+    const anios=req.body.anios;
+
+    db.query('insert into empleados(nombre,edad,pais,cargo,anios) values (?,?,?,?,?)',[nombre,edad,pais,cargo,anios],
+    (err, result)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+app.get("/empleados",(req,res)=>{
+    db.query('select * from empleados order by id desc',
+    (err, result)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+
+
+app.put("/update",(req,res)=>{
+    const id=req.body.id;
+    const nombre=req.body.nombre;
+    const edad=req.body.edad;
+    const pais=req.body.pais;
+    const cargo=req.body.cargo;
+    const anios=req.body.anios;
+
+    db.query('UPDATE empleados SET nombre=?,edad=?,pais=?,cargo=?,anios=? WHERE id=?',[nombre,edad,pais,cargo,anios,id],
+    (err, result)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+app.delete("/delete/:id",(req,res)=>{
+    const id=req.params.id;
+   
+
+    db.query('DELETE FROM  empleados WHERE id=?',id,
+    (err, result)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result);
+        }
+    });
+});
+
+app.listen(3001,()=>{
+    console.log('server is running on port 3001-237.44');
+})
+
+
+
